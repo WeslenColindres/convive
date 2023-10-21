c78734c2e981b910ad88252ce1d7bb2cb0f05121
WeslenColindres <weslencolindres100@gmail.com>
2023-10-21 09:27:00 -0600

    a

--- FerreteriaBackEnd/settings.py
+++ FerreteriaBackEnd/settings.py
@@ -137,4 +137,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  
+    "https://example.com",    
+    "http://*",               
+    "https://*",               
+]
