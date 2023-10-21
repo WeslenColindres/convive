583f6f962af6c3791ba922fa820b6b55a8b2884b
WeslenColindres <weslencolindres100@gmail.com>
2023-10-21 14:25:43 -0600

    d

--- api/views.py
+++ api/views.py
@@ -19,63 +19,63 @@ class UserList(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
 class UsuariosViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all() 
     serializer_class = UsuarioSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
 class CategoriaProductoViewSet(viewsets.ModelViewSet):
     queryset = CategoriaProducto.objects.all() 
     serializer_class = CategoriaProductoSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
 
 
 class ProveedorViewSet(viewsets.ModelViewSet):
     queryset = Proveedor.objects.all() 
     serializer_class = ProveedorSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
 class EntradaInventarioViewSet(viewsets.ModelViewSet):
     queryset = EntradaInventario.objects.prefetch_related('ID_Proveedor', 'ID_Producto').all() 
     serializer_class = EntradaInventarioSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
 
 class VentaViewSet(viewsets.ModelViewSet):
     queryset = Venta.objects.all()  
     serializer_class = VentaSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
 class VentaUsuarioViewSet(viewsets.ModelViewSet):
     queryset = VentaUsuario.objects.all()
     serializer_class = VentaUsuarioSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
 class DetalleVentaViewSet(viewsets.ModelViewSet):
     queryset = DetalleVenta.objects.all() 
     serializer_class = DetalleVentaSerializer
     pagination_class = LargeResultsSetPagination
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     
